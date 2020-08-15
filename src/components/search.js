0ffba18d9e61c8872c5da81cc5bcc3b29331facd
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-14 20:33:41 -0700

    Adding global state structure

--- src/components/search.js
+++ src/components/search.js
@@ -2,10 +2,18 @@ import React from "react";
 
 function Search() {
   return (
-    <div className="container">
-      <form>
-        <input type="text" placeholder="Search for an employee"></input>
-      </form>
+    <div className=" d-flex justify-content-center">
+      <div className="row">
+        <div className="col-sm-12">
+          <h1>Search for employee</h1>
+        </div>
+        <div className="d-flex justify-content-center">
+          <form>
+            <input type="text" placeholder="Search for an employee"></input>
+          </form>
+        </div>
+        
+      </div>
     </div>
   );
 }
