5ba33172c889d73a0512c4776b22c055a8de4240
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-14 21:01:58 -0700

    added list .js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Navbar from "./components/navbar"
-import Footer from "./components/footer"
-import Jumbotron from './components/jumbotron';
-import Search from "./components/search"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Jumbotron from "./components/jumbotron";
+import Search from "./components/search";
 
 function App() {
   return (
     <div className="">
-     <EmployeeProvider />
+      <Navbar />
+      <Jumbotron />
+   
+        <EmployeeList />
+
+      <Footer />
     </div>
   );
 }
