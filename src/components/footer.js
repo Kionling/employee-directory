51b407e5227490b3b880dc627280c6d2febdd5e9
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-14 17:44:30 -0700

    Creating employee template

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React from "react";
 
 function Footer() {
-    return(
-
-    );
+  return (
+    <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+      <div class="container text-center">
+        <small>Copyright &copy; Daniel Jauregui</small>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
---
+++ src/components/jumbotron.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Employee Directory</h1>
+        <p class="lead">
+          This application allows you to add and search users!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+
+export default Jumbotron
