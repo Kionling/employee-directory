7a4ec5765f5595d6b125b15c942fdaa9fc3effc5
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-14 22:51:07 -0700

    Generated conditions for states

--- src/components/togglelistorder.js
+++ src/components/togglelistorder.js
@@ -1,12 +1,44 @@
-import React from "react"
+import {useReducer, useEffect, useState} from "react"
 import employees from "../employees.json"
 import List from "../components/list"
 
-class App extends React.Component {
+function EmployeeSort(){
+//create 
+const [sort, setSort] = useState({
+    id: 0,
+    name: "",
+    phone: "",
+    department: ""
+})
 
+const [state, dispatch] = useReducer ((state, action) => {
+    if (action.type === "renderEmployees"){
+        return [...employees]
+    }
+    if (action.type === "descending"){
+        return sortDescend();
+    }
+    if (action.type === "ascending"){
+        return sortAscend()
+    }
+    else{
+        return state
+    }
+})
 
-
-
-
-    
+sortAscend = (employees, info) =>{
+    employees.sort(() => {
+        
+    })
 }
+
+
+
+
+
+
+
+
+}
+
+export default EmployeeSort
